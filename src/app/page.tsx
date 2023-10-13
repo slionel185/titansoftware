73dfaed494dba1fcacc26770447c43fb30013fc0
Seth Torrence <slionel1850@gmail.com>
2023-10-13 17:48:29 +0000

    cal.com adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function HomePage() {
                         <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
                             <span className='text-sm font-medium text-primary'>Step 1</span>
                             <span className='text-xl font-semibold'>Book your consultation</span>
-                            <span className='mt-2 text-muted-foreground'>Fill out our contact form or book through our Cal links now!</span>
+                            <span className='mt-2 text-muted-foreground'>Fill out our contact form or book through our cal.com links now!</span>
                         </div>
                     </li>
 
