8d3b5b4a8391f68f38e77b8ef551c81fd5cb7705
Seth Torrence <slionel1850@gmail.com>
2023-10-13 17:45:20 +0000

    Auth Removal

--- src/components/providers/MainProvider.tsx
+++ src/components/providers/MainProvider.tsx
@@ -1,15 +1,12 @@
-import AuthProvider from '@/components/providers/AuthProvider'
 import ThemeProvider from '@/components/providers/ThemeProvider'
 import QueryProvider from '@/components/providers/QueryProvider'
 
 export default function MainProvider({ children }: any) {
     return (
-        <AuthProvider>
-            <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-                <QueryProvider>
-                    {children}
-                </QueryProvider>
-            </ThemeProvider>
-        </AuthProvider>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+            <QueryProvider>
+                {children}
+            </QueryProvider>
+        </ThemeProvider>
     )
 }
