7d672c6ad8aba25986402762ee10836fd1f8cbd9
Seth Torrence <slionel1850@gmail.com>
2023-10-13 17:23:55 +0000

    Escaped Apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function HomePage() {
                         <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
                             <span className='text-sm font-medium text-primary'>Step 3</span>
                             <span className='text-xl font-semibold'>Go live</span>
-                            <span className='mt-2 text-muted-foreground'>That's it...</span>
+                            <span className='mt-2 text-muted-foreground'>That{'\''}s it...</span>
                         </div>
                     </li>
                 </ol>
