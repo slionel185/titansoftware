210c72fbc73a2da95dc8ecb170b9ff539d3783bc
Seth Torrence <slionel1850@gmail.com>
2023-10-17 22:04:27 +0000

    TRPC FIX

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -3,7 +3,7 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 import { appRouter } from '@/server'
 
 const handler = (req: Request) => fetchRequestHandler({
-    endpoint: '/api/trpc',
+    endpoint: 'https://titansoftware.dev/api/trpc',
     req,
     router: appRouter,
     createContext: () => ({})
