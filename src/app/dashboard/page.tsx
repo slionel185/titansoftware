7aeb52c2a53873770de4f55bd29cc05351f89d80
Seth Torrence <slionel1850@gmail.com>
2023-10-18 02:06:52 +0000

    Dashboard user information

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation'
 import { getServerSession } from 'next-auth'
 
-import LoaderPage from '@/components/pages/Loader'
 import MaxWidthContainer from '@/components/containers/MaxWidthContainer'
 
 export default async function AdminPage() {
@@ -9,20 +8,12 @@ export default async function AdminPage() {
 
     if(!session) return redirect('/')
 
-    if(session.user.user_type !== 'ADMIN') return (
-        <MaxWidthContainer className='mb-12 mt-12 flex flex-col justify-center items-center text-center bg-background'>
-                <h1 className='max-w-4xl text-5xl font-bold md:text-6xl text-foreground lg:text-7xl'>
-                    Coming soon!
-                </h1>
-                <p className='mt-8 text-muted-foreground'>Currently working on a way to allow all users to access and see the progress of their sites!</p>
-        </MaxWidthContainer>
-    )
-
     return (
         <MaxWidthContainer className='mb-12 mt-12 flex flex-col justify-center items-center text-center bg-background'>
                 <h1 className='max-w-4xl text-5xl font-bold md:text-6xl text-foreground lg:text-7xl'>
                     {session.user.email}
                 </h1>
+                <p className='mt-8 text-muted-foreground'>{session.user.user_type}</p>
         </MaxWidthContainer>
     )
 }
